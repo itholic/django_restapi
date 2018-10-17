a336d57314ccfbb7c7a71e568f026a939975624a
itholic <haejoon309@naver.com>
2018-10-17 15:16:47 +0900

    create model, serializer, view and add urls

--- tutorial/myapp/views.py
+++ tutorial/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PersonSerializer
+from myapp.models import Person
 
-# Create your views here.
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
