1351baab6a4849a48b70601da7a571a9e44b1ae9
itholic <haejoon309@naver.com>
2018-10-17 15:29:11 +0900

    fix for backwards compatible

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
 from myapp import views
@@ -23,7 +22,7 @@ router = routers.DefaultRouter()
 router.register(r'persons', views.PersonViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
